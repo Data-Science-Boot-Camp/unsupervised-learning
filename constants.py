fd84db8185afd5b16cbebdf89e7b0533fc3954a7
Edgar Ortiz Lenovo <ed.ortizm@gmail.com>
2023-05-22 21:13:58 -0400

    chore: update pca notebook with quesitons asked in class

---
+++ constants.py
@@ -0,0 +1,36 @@
+"""
+Constants
+"""
+
+STUDENTS_DS = [
+    "Claudio Bahamonde",
+    "Felipe Abarzúa",
+    "Diego Rodriguez",
+    "Javier Pizarro",
+    "Smilling Battaglini",
+    "Pamela Labbé",
+    "Marilina Fuentes",
+    "Jaime Castillo",
+    "Rodrigo Castillo",
+    "Ignacia Arenas",
+    "Luis reyes",
+    "Felipe Cabello",
+    "Polette Pino",
+    "Karen Lobos",
+    "Diego Ávila",
+    "Constanza Guzman",
+    "Nicole Alejandra Araya",
+    "Carlos Andrés Abarca",
+    "Marco Antonio Ramos",
+    "Claudia Araya",
+    "Daniela Hernández",
+    "Sergio Ananias",
+    "Alexis Bacian",
+    "Cristian Alvarez",
+    "Nubia Ascencio",
+    "Veronica Marín",
+    "Liliana Romina Garmendia",
+    "Rocio Macari",
+    "Loreto Llambias",
+    "Braulio Valdés",
+]
